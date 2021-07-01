f50ab7485b4b8453cf604a0ab334bf42b3c3123d
May Phoo Wint Zay Oo <mayphoowintzayoo@gmail.com>
2021-07-01 17:15:18 +0630

    Update Match section page

--- pages/Match.js
+++ pages/Match.js
@@ -1,23 +1,164 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { Text, StyleSheet,View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const Match = ({route}) => {
-  const {unitId} = route.params;
-  const {vocabularyData} = route.params;
+  const [open, setOpen] = useState(false);
+  const [openFirst, setOpenFirst] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'}
+  ]);
+
   return (
-    <Text style={styles.baseText}>
-      {vocabularyData[0].voc_mm}
-      <Text style={styles.innerText}> and red</Text>
-    </Text>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+      </View>
+      <View style = {styles.detailcontainer}>
+        <View style={styles.separatorTextContainer}>
+            <View style={styles.textContainerWithoutMarginTop}>
+              <Text>Hello World</Text>
+            </View>
+            <View style={styles.textContainer}>
+              <Text>Hello World</Text>
+            </View>
+            <View style={styles.textContainer}>
+              <Text>Hello World</Text>
+            </View>
+            <View style={styles.textContainer}>
+              <Text>Hello World</Text>
+            </View>
+            <View style={styles.textContainer}>
+              <Text>Hello World</Text>
+            </View>
+        </View>
+        <View style={styles.separatorContainer}>
+            <DropDownPicker
+            open={openFirst}
+           value={value}
+           items={items}
+           setOpen={setOpenFirst}
+           setValue={setValue}
+           setItems={setItems}
+          zIndex={50}
+         />
+         <DropDownPicker
+          open={open}
+           value={value}
+         items={items}
+         style={styles.dropDownContainer}
+         setOpen={setOpen}
+         setValue={setValue}
+         setItems={setItems}
+        zIndex={40}
+        />
+       <DropDownPicker
+         open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        style={styles.dropDownContainer}
+         setValue={setValue}
+         setItems={setItems}
+        zIndex={30}
+      />
+       <DropDownPicker
+         open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        style={styles.dropDownContainer}
+         setValue={setValue}
+         setItems={setItems}
+        zIndex={20}
+      />
+       <DropDownPicker
+         open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        style={styles.dropDownContainer}
+         setValue={setValue}
+         setItems={setItems}
+        zIndex={10}
+      />
+    </View>
+   </View>
+      
+  </View>
+    
   );
 };
 
 const styles = StyleSheet.create({
-  baseText: {
-    fontWeight: 'bold'
+  container: {
+    flex: 1,
+    padding: 5,
+    borderWidth: 1,
+    borderRadius: 5,
+    flexDirection:'column'
+   },
+   titleContainer:{
+    flex: 0.05,
+    flexDirection:'column',
+    backgroundColor:"blue",
+   },
+   detailcontainer:{
+     flex: 0.4,
+     width:"100%",
+     flexDirection:'row',    
+   },
+   styleDetailContainer:{
+    flex: 0.5,
+    flexDirection:'column',
+    backgroundColor:"yellow"
+
+   },
+  dropDownContainer:{
+    marginTop:10,
+    width:"100%",
+    justifyContent:'center',
+    alignItems:'center',
+    textAlign:"center"
   },
-  innerText: {
-    color: 'red'
+   separatorTextContainer:{
+    width:"40%",
+    justifyContent:'center',
+    alignItems:'center',
+    marginHorizontal:5
+    
+  },
+  separatorContainer:{
+    marginTop:10,
+    width:"56%",
+    justifyContent:'center',
+    alignItems:'center',
+    marginHorizontal:5
+  },
+  textContainer:{
+    width:"100%",
+    height:"16%",
+    marginTop:11,
+    marginHorizontal:10,
+    textAlign:'center',
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    
+  },
+  textContainerWithoutMarginTop:{
+    width:"100%",
+    height:"16%",
+    marginTop:12,
+    marginHorizontal:10,
+    textAlign:'center',
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth: 1,
+    borderRadius: 5,
+   
   }
 });
 
